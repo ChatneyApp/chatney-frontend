8543c3a1dea76428518c5c257f20d4cb778be4ab
Denis Klyuev <denis.klyuev@gmail.com>
2026-05-30 01:12:20 -0400

    feat: more components refactoring

---
+++ src/pages/client/Chat/ReactionSelectionDialog/index.ts
@@ -0,0 +1,1 @@
+export { ReactionSelectionDialog } from './ReactionSelectionDialog';
--- src/pages/client/Chat/WorkspacesList/WorkspacesList.tsx
+++ src/pages/client/Chat/WorkspacesList/WorkspacesList.tsx
@@ -1,45 +1,22 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState } from 'react';
 import clsx from 'clsx';
 
-import { useUser } from '@/contexts/UserContext';
 import { useWorkspacesList } from '@/contexts/WorkspacesListContext';
 import { Workspace } from '@/types/workspaces';
 import { WorkspaceCreateModal } from '../WorkspaceCreateModal';
+import { UserSettingsPopup } from '../UserSettingsPopup';
 
 import styles from './WorkspacesList.module.css';
 
 export function WorkspacesList() {
     const [isModalOpen, setIsModalOpen] = useState(false);
     const workspacesList = useWorkspacesList();
-    const userCtx = useUser();
-    const [showPopup, setShowPopup] = useState(false);
-    const popupRef = useRef<HTMLDivElement>(null);
-    const avatarRef = useRef<HTMLDivElement>(null);
 
     const handleWorkspaceCreated = (newWs: Workspace) => {
         workspacesList.setWorkspacesList([...workspacesList.workspacesList, newWs]);
         workspacesList.setActiveWorkspaceId(newWs.id);
         setIsModalOpen(false);
     };
-
-    const handleClickOutside = (event: MouseEvent) => {
-        if ((popupRef.current && !popupRef.current.contains(event.target as Node)) &&
-            (avatarRef.current && !avatarRef.current.contains(event.target as Node))) {
-            setShowPopup(false);
-        }
-    };
-
-    useEffect(() => {
-        if (showPopup) {
-            document.addEventListener('mouseup', handleClickOutside);
-        } else {
-            document.removeEventListener('mouseup', handleClickOutside);
-        }
-
-        return () => {
-            document.removeEventListener('mouseup', handleClickOutside);
-        };
-    }, [showPopup]);
 
     return (
         <div className={styles.container}>
@@ -75,59 +52,7 @@ export function WorkspacesList() {
                 )}
             </div>
 
-            <div className={styles.profileArea}>
-                {showPopup && (
-                    <div
-                        ref={popupRef}
-                        className={styles.profilePopup}
-                    >
-                        <div className={styles.profilePopupChevron} />
-
-                        <div className={styles.profileInfo}>
-                            <div className={styles.profileName}>{userCtx?.user?.name}</div>
-                            <div className={styles.profileRole}>Role</div>
-                        </div>
-
-                        <ul className={styles.profileLinks}>
-                            <li>
-                                <a href="/dashboard" className={styles.profileLink}>
-                                    Dashboard
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/settings" className={styles.profileLink}>
-                                    Settings
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/profile" className={styles.profileLink}>
-                                    Profile
-                                </a>
-                            </li>
-                            <li>
-                                <a href="/help" className={styles.profileLink}>
-                                    Help & Support
-                                </a>
-                            </li>
-                            <br />
-                            <li>
-                                <a onClick={() => userCtx?.logout()} className={styles.logoutLink}>
-                                    Logout
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                )}
-
-                <div
-                    ref={avatarRef}
-                    className={styles.avatar}
-                    title="Your Profile"
-                    onClick={() => setShowPopup(!showPopup)}
-                >
-                    {userCtx?.user?.name[0].toUpperCase()}
-                </div>
-            </div>
+            <UserSettingsPopup />
         </div>
     );
 }
